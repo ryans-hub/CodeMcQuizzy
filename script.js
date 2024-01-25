50c9f78fb20b702371299917d5cff8c77cb338b4
PanMan <ryanispan@gmail.com>
2024-01-24 21:30:37 -0700

    added function to check answer, and penalize on incorrect answer, updated index.html file to function with displaying answers

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ var challengeBtn = document.querySelector("#challenge");
 var timeEl = document.querySelector(".time");
 var parentEl = document.querySelector("#parent");
 var childEl = document.querySelector("#child");
+var answerEl = document.querySelector("#answer");
 var secondsLeft = 75;
 
 var question = "What is my favorite color?";
 var answer = "Purple";
+var answer2 = "Green";
 
 challengeBtn.addEventListener("click", startQuiz);
 
 function startQuiz() {
 
-    // Removes the starting challenge button from index.html file
+    // Removes the starting challenge button from index.html
     challengeBtn.style.display = "none";
+    childEl.style.display = "none";
     startTimer();
-    displayQuestion();
+    displayQuiz();
     
 }
 
@@ -32,12 +35,35 @@ function startTimer() {
 
 }
 
-
-
-function displayQuestion() {
+function displayQuiz() {
 
     parentEl.textContent = question;
-    childEl.textContent = answer;
+
+    var answerBtn = document.createElement("button");
+    answerBtn.textContent = answer;
+    answerEl.appendChild(answerBtn);
+
+    var answerBtn2 = document.createElement("button");
+    answerBtn2.textContent = answer2;
+    answerEl.appendChild(answerBtn2);
     
+    answerBtn.addEventListener("click", checkAnswer);
+    answerBtn2.addEventListener("click", checkAnswer);
+    
+
+}
+
+function checkAnswer(event) {
+
+    var selected = event.target.textContent;
+
+    if(selected === answer) {
+
+        console.log("correct");
+
+    } else {
+        console.log("incorrect");
+        secondsLeft-=10;
+    }
 }
 
